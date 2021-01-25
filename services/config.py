0aacc9a51ad87a1b57cfdc71026459bdc63b9f9d
yusuketk <yusuketk@weseek.co.jp>
2021-01-25 12:25:16 +0900

    imprv config

--- services/config.py
+++ services/config.py
@@ -10,50 +10,65 @@ class ConfigService:
     def __init__(self, services):
         self.services = services
         self.prize = [20, 10, -10, -20]
-        self.configs = {'レート': '点3', '順位点': str(
-            self.prize), '飛び賞': 10, 'チップ': 'なし', '人数': 4}
+        self.configs = {'レート': '点3', '順位点': self.prize,
+                        '飛び賞': 10, 'チップ': 'なし', '人数': 4,
+                        '計算方法': '3万点以下切り上げ/以上切り捨て'}
 
-    def get_by_target(self):
-        # configs = self.services.app_service.db.session\
-        #     .query(Configs)\
-        #     .filter(Configs.target_id == self.services.app_service.req_room_id)\
-        #     .all()
-        # print(configs)
+    def get_all(self):
+        configs = self.services.app_service.db.session\
+            .query(Configs)\
+            .filter(Configs.target_id == self.services.app_service.req_room_id)\
+            .all()
         res = self.configs
-        # for config in configs:
-        # res[key] = value
+        for config in configs:
+            res[config.key] = config.value
         return res
 
     def reply(self):
         configs = self.get_by_target()
-        s = [f'{key}: {value}' for key, value in configs.items()]
+        s = [f'{key}: {str(value)}' for key, value in configs.items()]
         self.services.reply_service.add_text('[設定]\n' + '\n'.join(s))
 
-    def get_rate(self):
-        configs = self.get_by_target()
-        return int(configs['レート'][1:]) * 10
+    # def get_rate(self):
+    #     configs = self.get_by_target()
+    #     return int(configs['レート'][1:]) * 10
 
-    def update(self, key, value, insert=True):
+    def get(self, key):
+        target_id = self.services.app_service.req_room_id
         config = self.services.app_service.db.session\
             .query(Configs)\
             .filter(and_(
-                Configs.target_id == self.services.app_service.req_room_id,
+                Configs.target_id == target_id,
                 Configs.key == key,
-                Configs.value == value,
             ))\
             .first()
-        if config == None:
-            if insert == True:
-                self.add(key, value)
-            return
-        config.value = value
-        self.services.app_service.db.session.commit()
+        if config is None:
+            return self.configs[key]
+        return config.value
 
-    def add(self, key, value):
+    def update(self, key, value):
         target_id = self.services.app_service.req_room_id
-        config = Configs(target_id=target_id,
-                         key=key,
-                         value=value,
-                         )
-        self.services.app_service.db.session.add(config)
+        self.services.app_service.db.session\
+            .query(Configs)\
+            .filter(and_(
+                Configs.target_id == target_id,
+                Configs.key == key,
+            ))\
+            .delete()
+        # self.services.app_service.db.session.commit()
+        if value != self.configs[key]:
+            config = Configs(target_id=target_id,
+                             key=key,
+                             value=value,
+                             )
+            self.services.app_service.db.session.add(config)
         self.services.app_service.db.session.commit()
+        return
+
+    def reply_all_records(self):
+        # configs = self.services.app_service.db.session\
+        #     .query(Configs).all()
+        # if len(configs) == 0:
+        #     configs = ['なし']
+        # self.services.reply_service.add_text('\n\n'.join(configs))
+        return
