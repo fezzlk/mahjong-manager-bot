279c46b05990e36f30e73cb7654f716b69bad9fb
yusuketk <yusuketk@weseek.co.jp>
2021-02-18 10:55:32 +0900

    test

--- test/main.py
+++ test/main.py
@@ -1,4 +1,15 @@
-def test():
+from router import Router
+from services import Services
+
+
+def test_hoge():
     a = 1
     b = 1
+    assert a == b
+
+
+def test_fuga():
+    a = 1
+    b = 1
+    Router.root()
     assert a == b
