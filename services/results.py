6832cb107d91573f2a4a4224cfb6468db6dd7cab
yusuketk <yusuketk@weseek.co.jp>
2021-04-21 11:49:55 +0900

    sort ids

--- services/results.py
+++ services/results.py
@@ -95,6 +95,8 @@ class ResultsService:
         )
 
     def reply_sum_and_money_by_ids(self, ids, match_id, is_required_sum=True, date=''):
+        if type(match_id) == list:
+            match_id.sort()
         results = self.services.app_service.db.session\
             .query(Results).filter(
                 Results.id.in_([int(s) for s in ids]),
