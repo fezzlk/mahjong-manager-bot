cfcd254b363d0f3c4cc50a544edabbf6ef5f0ff1
fezzlk <fezzlk796@gmail.com>
2021-07-23 18:30:14 +0900

    comment services/app.py

--- services/app.py
+++ services/app.py
@@ -7,10 +7,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 class AppService:
-    """app service"""
+    """
+    app service
+
+    management following
+    - logger service
+    - line bot api instance
+    - ID of LINE user or talk room which this server recieve message from
+    - connection to DB with SQLAlchemy
+    """
 
     def __init__(self, service, app):
+        # logger
         self.logger = logging.create_logger(app)
+
+        # line bot api
         self.line_bot_api = None
         if "YOUR_CHANNEL_ACCESS_TOKEN" in os.environ:
             YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -18,22 +29,25 @@ class AppService:
         else:
             self.logger.warning(
                 'line_bot_api is not setup because YOUR_CHANNEL_ACCESS_TOKEN is not found.')
+
+        # ID of LINE user, LINE room to talk to
         self.req_user_id = None
         self.req_room_id = None
 
+        # connection to DB
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         self.db = SQLAlchemy(app)
 
     def set_req_info(self, event):
-        """set request info"""
+        """set request infomation from LINE event"""
 
         self.req_user_id = event.source.user_id
         if event.source.type == 'room':
             self.req_room_id = event.source.room_id
 
     def delete_req_info(self):
-        """delete request info"""
+        """delete request infomation"""
 
         self.req_user_id = None
         self.req_room_id = None
