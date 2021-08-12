cd1958353267b84de7355358290d9d902a2e1bfc
fezzlk <fezzlk796@gmail.com>
2021-08-13 04:24:28 +0900

    adjust api.py

--- src/api.py
+++ src/api.py
@@ -2,12 +2,17 @@
 api
 """
 
-from server import app
+from server import app, Engine
 
+    """get results
+
+    Returns:
+        data: results list
+    """
 @app.route('/_api/results')
 def api_get_results():
-    # Results.get_json(Engine)
-    # data = services.results_service.get()
-    # print(data)
-    # print(type(data))
-    return 'hoge'
+    Results.get_json(Engine)
+    data = services.results_service.get()
+    print(data)
+    print(type(data))
+    return data
