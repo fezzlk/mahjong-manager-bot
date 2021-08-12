6a50439a76e8e7477a45d6243458adca26dc3e01
fezzlk <fezzlk796@gmail.com>
2021-08-13 04:24:53 +0900

    adjust server.py

--- src/server.py
+++ src/server.py
@@ -7,21 +7,20 @@ import os
 import sys
 sys.path.append("/src")
 
-from db_setting import Base, Engine
 from flask import Flask
-from linebot import WebhookHandler
-
-from router import Router
-from services import Services
-
-"""define LINE bot handler"""
-handler = WebhookHandler(os.environ["YOUR_CHANNEL_SECRET"])
-
 app = Flask(__name__)
 
-services = Services(app)
-router = Router(services)
+from linebot import WebhookHandler
+handler = WebhookHandler(os.environ["YOUR_CHANNEL_SECRET"])
+
+from db_setting import Base, Engine
 Base.metadata.create_all(bind=Engine)
+
+from services import Services
+services = Services(app)
+
+from router import Router
+router = Router(services)
 
 import views
 import api
