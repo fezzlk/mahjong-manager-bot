565b1fea634b4522ffa30b85994446adf7116cb4
fezzlk <fezzlk796@gmail.com>
2021-08-15 00:18:14 +0900

    user

---
+++ src/domain.py/user.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from domain.match import Match
+from domain.room import Room
+
+
+@dataclass(frozen=True)
+class User:
+    id: int
+    name: str
+    user_id: str
+    zoom_id: str
+    mode: str
+    jantama_name: str
+    matches: [Match]
+    rooms: [Room]
