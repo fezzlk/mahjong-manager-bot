83cf0ceb2bc3d2440ef6013072abb64269a7e99d
fezzlk <fezzlk796@gmail.com>
2021-08-15 00:17:46 +0900

    hanchan

---
+++ src/domain.py/match.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+import datetime
+
+from domain.user import User
+
+
+@dataclass(frozen=True)
+class Match:
+    id: int
+    room_id: str
+    result_ids: str
+    status: int
+    users: [User]
+    status: int
+    create_at: datetime.date
