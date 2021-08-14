160368ec4d075c494c0159d82a08a3e66a576f3a
fezzlk <fezzlk796@gmail.com>
2021-08-14 19:05:19 +0900

    adjust importing

--- src/api.py
+++ src/api.py
@@ -2,15 +2,18 @@
 api
 """
 
-from server import app, Engine
+from server import app, services
+from db_setting import Engine
+from models import Results
 
-"""get results
 
-Returns:
-    data: results list
-"""
 @app.route('/_api/results')
 def api_get_results():
+    """get results
+
+    Returns:
+        data: results list
+    """
     Results.get_json(Engine)
     data = services.results_service.get()
     print(data)
