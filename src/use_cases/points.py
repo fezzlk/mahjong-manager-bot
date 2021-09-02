2442215917ad4003be17067d2d4fe0ee3967ab70
fezzlk <fezzlk796@gmail.com>
2021-09-02 22:30:00 +0900

    clean code

--- src/use_cases/points.py
+++ src/use_cases/points.py
@@ -78,14 +78,3 @@ class PointsUseCases:
             return s[-1], ' '.join(s[:-1])
         elif len(s) == 1:
             return 'delete', s[0]
-
-    def add_by_ocr(self):
-        results = ocr_service.get_points()
-        if results is None:
-            return
-
-        res_message = "\n".join([
-            f'{user}: {(point//100)*100}' for user, point in results.items()
-        ])
-        reply_service.add_message(res_message)
-        reply_service.add_submit_results_by_ocr_menu(results)
