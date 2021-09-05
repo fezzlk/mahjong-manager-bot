6763d2cbf79224d04792db55b39ad8dbca28bab6
fezzlk <fezzlk796@gmail.com>
2021-09-05 20:59:53 +0900

    🐛 import not existed module

--- src/use_cases/__init__.py
+++ src/use_cases/__init__.py
@@ -3,7 +3,6 @@ from .calculate import CalculateUseCases
 from .config import ConfigUseCases
 from .hanchans import HanchansUseCases
 from .matches import MatchesUseCases
-from .message import MessageUseCases
 from .ocr import OcrUseCases
 from .points import PointsUseCases
 from .room import RoomUseCases
@@ -14,7 +13,6 @@ calculate_use_cases = CalculateUseCases()
 config_use_cases = ConfigUseCases()
 hanchans_use_cases = HanchansUseCases()
 matches_use_cases = MatchesUseCases()
-message_use_cases = MessageUseCases()
 ocr_use_cases = OcrUseCases()
 points_use_cases = PointsUseCases()
 room_use_cases = RoomUseCases()
