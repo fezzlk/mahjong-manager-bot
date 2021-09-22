6784e1bceb772dbb1d7bc379d6bf43ef32296aed
fezzlk <fezzlk796@gmail.com>
2021-09-23 02:29:20 +0900

    create ut of hanchan repository

--- src/repositories/tests/HanchanRepository/test_hanchan_repository_delete_by_ids.py
+++ src/repositories/tests/HanchanRepository/test_hanchan_repository_delete_by_ids.py
@@ -1,105 +1,131 @@
-# from tests.dummies import generate_dummy_room_list
-# from db_setting import Session
-# from repositories import session_scope
-# from repositories.room_repository import RoomRepository
-# from domains.room import Room
+from tests.dummies import generate_dummy_hanchan_list, generate_dummy_match_list
+from db_setting import Session
+from repositories import session_scope
+from repositories.HanchanRepository import HanchanRepository
+from repositories.MatchRepository import MatchRepository
+from domains.Hanchan import Hanchan
 
-# session = Session()
-
-
-# def test_hit_with_ids():
-#     # Arrange
-#     dummy_rooms = generate_dummy_room_list()[:3]
-#     with session_scope() as session:
-#         for dummy_room in dummy_rooms:
-#             RoomRepository.create(
-#                 session,
-#                 dummy_room,
-#             )
-#     other_rooms = dummy_rooms[:1]
-#     target_rooms = dummy_rooms[1:3]
-#     ids = [target_room._id for target_room in target_rooms]
-
-#     # Act
-#     with session_scope() as session:
-#         RoomRepository.delete_by_ids(
-#             session,
-#             ids,
-#         )
-
-#     # Assert
-#     with session_scope() as session:
-#         result = RoomRepository.find_all(
-#             session,
-#         )
-#         assert len(result) == len(other_rooms)
-#         for i in range(len(result)):
-#             assert isinstance(result[i], Room)
-#             assert result[i].line_room_id == other_rooms[i].line_room_id
-#             assert result[i].zoom_url == other_rooms[i].zoom_url
-#             assert result[i].mode == other_rooms[i].mode
+session = Session()
 
 
-# def test_hit_with_an_id_as_not_list():
-#     # Arrange
-#     dummy_rooms = generate_dummy_room_list()[:3]
-#     with session_scope() as session:
-#         for dummy_room in dummy_rooms:
-#             RoomRepository.create(
-#                 session,
-#                 dummy_room,
-#             )
-#     other_rooms = dummy_rooms[:2]
-#     target_room = dummy_rooms[2]
-#     target_room_id = target_room._id
+def test_hit_with_ids():
+    # Arrange
+    dummy_matches = generate_dummy_match_list()
+    with session_scope() as session:
+        for dummy_match in dummy_matches:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+    dummy_hanchans = generate_dummy_hanchan_list()[:3]
+    with session_scope() as session:
+        for dummy_hanchan in dummy_hanchans:
+            HanchanRepository.create(
+                session,
+                dummy_hanchan,
+            )
+    other_hanchans = dummy_hanchans[:1]
+    target_hanchans = dummy_hanchans[1:3]
+    ids = [target_hanchan._id for target_hanchan in target_hanchans]
 
-#     # Act
-#     with session_scope() as session:
-#         result = RoomRepository.delete_by_ids(
-#             session,
-#             target_room_id,
-#         )
+    # Act
+    with session_scope() as session:
+        HanchanRepository.delete_by_ids(
+            session,
+            ids,
+        )
 
-#     # Assert
-#     with session_scope() as session:
-#         result = RoomRepository.find_all(
-#             session,
-#         )
-#         assert len(result) == len(other_rooms)
-#         for i in range(len(result)):
-#             assert isinstance(result[i], Room)
-#             assert result[i].line_room_id == other_rooms[i].line_room_id
-#             assert result[i].zoom_url == other_rooms[i].zoom_url
-#             assert result[i].mode == other_rooms[i].mode
+    # Assert
+    with session_scope() as session:
+        result = HanchanRepository.find_all(
+            session,
+        )
+        assert len(result) == len(other_hanchans)
+        for i in range(len(result)):
+            assert isinstance(result[i], Hanchan)
+            assert result[i].line_room_id == other_hanchans[i].line_room_id
+            assert result[i].match_id == other_hanchans[i].match_id
+            assert result[i].raw_scores == other_hanchans[i].raw_scores
+            assert result[i].converted_scores == other_hanchans[i].converted_scores
+            assert result[i].status == other_hanchans[i].status
 
 
-# def test_hit_0_record():
-#     # Arrange
-#     with session_scope() as session:
-#         dummy_rooms = generate_dummy_room_list()[:3]
-#         for dummy_room in dummy_rooms:
-#             RoomRepository.create(
-#                 session,
-#                 dummy_room,
-#             )
-#     target_rooms = generate_dummy_room_list()[3:6]
-#     ids = [target_room._id for target_room in target_rooms]
+def test_hit_with_an_id_as_not_list():
+    # Arrange
+    dummy_matches = generate_dummy_match_list()
+    with session_scope() as session:
+        for dummy_match in dummy_matches:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+    dummy_hanchans = generate_dummy_hanchan_list()[:3]
+    with session_scope() as session:
+        for dummy_hanchan in dummy_hanchans:
+            HanchanRepository.create(
+                session,
+                dummy_hanchan,
+            )
+    other_hanchans = dummy_hanchans[:2]
+    target_hanchan = dummy_hanchans[2]
+    target_hanchan_id = target_hanchan._id
 
-#     # Act
-#     with session_scope() as session:
-#         result = RoomRepository.delete_by_ids(
-#             session,
-#             ids,
-#         )
+    # Act
+    with session_scope() as session:
+        result = HanchanRepository.delete_by_ids(
+            session,
+            target_hanchan_id,
+        )
 
-#     # Assert
-#     with session_scope() as session:
-#         result = RoomRepository.find_all(
-#             session,
-#         )
-#         assert len(result) == len(dummy_rooms)
-#         for i in range(len(result)):
-#             assert isinstance(result[i], Room)
-#             assert result[i].line_room_id == dummy_rooms[i].line_room_id
-#             assert result[i].zoom_url == dummy_rooms[i].zoom_url
-#             assert result[i].mode == dummy_rooms[i].mode
+    # Assert
+    with session_scope() as session:
+        result = HanchanRepository.find_all(
+            session,
+        )
+        assert len(result) == len(other_hanchans)
+        for i in range(len(result)):
+            assert isinstance(result[i], Hanchan)
+            assert result[i].line_room_id == other_hanchans[i].line_room_id
+            assert result[i].match_id == other_hanchans[i].match_id
+            assert result[i].raw_scores == other_hanchans[i].raw_scores
+            assert result[i].converted_scores == other_hanchans[i].converted_scores
+            assert result[i].status == other_hanchans[i].status
+
+
+def test_hit_0_record():
+    # Arrange
+    dummy_matches = generate_dummy_match_list()
+    with session_scope() as session:
+        for dummy_match in dummy_matches:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+    with session_scope() as session:
+        dummy_hanchans = generate_dummy_hanchan_list()[:3]
+        for dummy_hanchan in dummy_hanchans:
+            HanchanRepository.create(
+                session,
+                dummy_hanchan,
+            )
+
+    # Act
+    with session_scope() as session:
+        result = HanchanRepository.delete_by_ids(
+            session,
+            [4, 5],
+        )
+
+    # Assert
+    with session_scope() as session:
+        result = HanchanRepository.find_all(
+            session,
+        )
+        assert len(result) == len(dummy_hanchans)
+        for i in range(len(result)):
+            assert isinstance(result[i], Hanchan)
+            assert result[i].line_room_id == dummy_hanchans[i].line_room_id
+            assert result[i].match_id == dummy_hanchans[i].match_id
+            assert result[i].raw_scores == dummy_hanchans[i].raw_scores
+            assert result[i].converted_scores == dummy_hanchans[i].converted_scores
+            assert result[i].status == dummy_hanchans[i].status
