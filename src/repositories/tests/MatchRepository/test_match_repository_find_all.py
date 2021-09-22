9b833dcd975f5ffb8f8088ef47bc3829dda01e33
fezzlk <fezzlk796@gmail.com>
2021-09-23 01:16:08 +0900

    create ut of find all

---
+++ src/repositories/tests/MatchRepository/test_match_repository_find_all.py
@@ -0,0 +1,47 @@
+from tests.dummies import generate_dummy_match_list
+from db_setting import Session
+from repositories import session_scope
+from repositories.MatchRepository import MatchRepository
+from domains.Match import Match
+
+session = Session()
+
+
+def test_success_find_records():
+    # Arrange
+    with session_scope() as session:
+        dummy_matchs = generate_dummy_match_list()
+        for dummy_match in dummy_matchs:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+
+    # Act
+    with session_scope() as session:
+        result = MatchRepository.find_all(
+            session,
+        )
+
+    # Assert
+        assert len(result) == len(dummy_matchs)
+        for i in range(len(result)):
+            assert isinstance(result[i], Match)
+            assert result[i].line_room_id == dummy_matchs[i].line_room_id
+            assert result[i].hanchan_ids == dummy_matchs[i].hanchan_ids
+            assert result[i].users == dummy_matchs[i].users
+            assert result[i].status == dummy_matchs[i].status
+
+
+def test_success_find_0_record():
+    # Arrange
+    # Do nothing
+
+    # Act
+    with session_scope() as session:
+        result = MatchRepository.find_all(
+            session,
+        )
+
+    # Assert
+        assert len(result) == 0
