2e109ff3c4db82558d4e587316fc7ee22e8ec070
fezzlk <fezzlk796@gmail.com>
2021-09-23 01:18:35 +0900

    create ut of find by ids

---
+++ src/repositories/tests/MatchRepository/test_match_repository_find_by_ids.py
@@ -0,0 +1,86 @@
+from tests.dummies import generate_dummy_match_list
+from db_setting import Session
+from repositories import session_scope
+from repositories.MatchRepository import MatchRepository
+from domains.Match import Match
+
+session = Session()
+
+
+def test_hit_with_ids():
+    # Arrange
+    with session_scope() as session:
+        dummy_matchs = generate_dummy_match_list()[:3]
+        for dummy_match in dummy_matchs:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+    target_matchs = generate_dummy_match_list()[1:3]
+    ids = [target_match._id for target_match in target_matchs]
+
+    # Act
+    with session_scope() as session:
+        result = MatchRepository.find_by_ids(
+            session,
+            ids,
+        )
+
+    # Assert
+        assert len(result) == len(target_matchs)
+        for i in range(len(result)):
+            assert isinstance(result[i], Match)
+            assert result[i].line_room_id == target_matchs[i].line_room_id
+            assert result[i].hanchan_ids == target_matchs[i].hanchan_ids
+            assert result[i].users == target_matchs[i].users
+            assert result[i].status == target_matchs[i].status
+
+
+def test_hit_with_an_id_as_not_list():
+    # Arrange
+    with session_scope() as session:
+        dummy_matchs = generate_dummy_match_list()[:3]
+        for dummy_match in dummy_matchs:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+    target_match = generate_dummy_match_list()[2]
+    target_match_id = target_match._id
+
+    # Act
+    with session_scope() as session:
+        result = MatchRepository.find_by_ids(
+            session,
+            target_match_id,
+        )
+
+    # Assert
+        assert len(result) == 1
+        assert result[0].line_room_id == dummy_match.line_room_id
+        assert result[0].hanchan_ids == dummy_match.hanchan_ids
+        assert result[0].users == dummy_match.users
+        assert result[0].status == dummy_match.status
+
+
+def test_hit_0_record():
+    # Arrange
+    with session_scope() as session:
+        dummy_matchs = generate_dummy_match_list()[:3]
+        for dummy_match in dummy_matchs:
+            MatchRepository.create(
+                session,
+                dummy_match,
+            )
+    target_matchs = generate_dummy_match_list()[3:6]
+    ids = [target_match._id for target_match in target_matchs]
+
+    # Act
+    with session_scope() as session:
+        result = MatchRepository.find_by_ids(
+            session,
+            ids,
+        )
+
+    # Assert
+        assert len(result) == 0
