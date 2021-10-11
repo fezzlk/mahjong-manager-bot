1afd5df606bd9b34740fb1fab5865802d824ba97
fezzlk <fezzlk796@gmail.com>
2021-10-11 23:43:07 +0900

    prevent to create duplicate hanchan

--- src/use_cases/StartInputUseCase.py
+++ src/use_cases/StartInputUseCase.py
@@ -12,14 +12,19 @@ from domains.Room import RoomMode
 class StartInputUseCase:
 
     def execute(self):
-        room_id = request_info_service.req_line_room_id
-        current_match = match_service.get_or_add_current(room_id)
-        hanchan_service.add({}, room_id, current_match)
+        line_room_id = request_info_service.req_line_room_id
+        
+        if room_service.get_mode(line_room_id) == RoomMode.input:
+            reply_service.add_message('すでに入力モードです')
+            return
+            
+        current_match = match_service.get_or_create_current(line_room_id)
+        hanchan_service.add({}, line_room_id, current_match)
 
         room_service.chmod(
-            room_id,
+            line_room_id,
             RoomMode.input,
         )
         reply_service.add_message(
-            f'第{match_service.count_results(room_id)+1}回戦お疲れ様です。各自点数を入力してください。\
+            f'第{match_service.count_results(line_room_id)+1}回戦お疲れ様です。各自点数を入力してください。\
             \n（同点の場合は上家が高くなるように数点追加してください）')
