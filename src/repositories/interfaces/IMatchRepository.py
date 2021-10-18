227f16065bded85a8f32aa0a4c5ab86c8bc37b97
fezzlk <fezzlk796@gmail.com>
2021-10-18 23:28:47 +0900

    typing match repository

--- src/repositories/interfaces/IMatchRepository.py
+++ src/repositories/interfaces/IMatchRepository.py
@@ -1,64 +1,82 @@
 from abc import ABCMeta, abstractmethod
+from domains.Match import Match
+from sqlalchemy.orm.session import Session as BaseSession
 
 
-class IConfigRepository(metaclass=ABCMeta):
+class IMatchRepository(metaclass=ABCMeta):
     @abstractmethod
-    def find_by_ids(self, session, ids):
+    def find_by_ids(
+        self,
+        session: BaseSession,
+        ids: list,
+    ) -> list:
         pass
 
     @abstractmethod
     def find_one_by_line_room_id_and_status(
         self,
-        session,
-        line_room_id,
-        status,
-    ):
+        session: BaseSession,
+        line_room_id: str,
+        status: int,
+    ) -> Match:
         pass
 
     @abstractmethod
-    def find_many_by_room_id_and_status(self, session, line_room_id, status):
+    def find_many_by_room_id_and_status(
+        self,
+        session: BaseSession,
+        line_room_id: str,
+        status: int
+    ) -> list:
         pass
 
     @abstractmethod
-    def create(self, session, new_match):
+    def create(
+        self,
+        session: BaseSession,
+        new_match: Match,
+    ) -> None:
         pass
 
     @abstractmethod
-    def find_all(self, session):
+    def find_all(
+        self,
+        session: BaseSession,
+    ) -> list:
         pass
 
     @abstractmethod
     def add_hanchan_id_by_line_room_id(
         self,
-        session,
-        line_room_id,
-        hanchan_id,
-    ):
+        session: BaseSession,
+        line_room_id: str,
+        hanchan_id: int,
+    ) -> Match:
         pass
 
     @abstractmethod
     def update_one_status_by_line_room_id(
         self,
-        session,
-        line_room_id,
-        status,
-    ):
+        session: BaseSession,
+        line_room_id: str,
+        status: int,
+    ) -> Match:
         pass
 
     @abstractmethod
     def update_one_hanchan_ids_by_line_room_id(
         self,
-        session,
-        line_room_id,
-        hanchan_ids,
-    ):
+        session: BaseSession,
+        line_room_id: str,
+        hanchan_ids: list,
+    ) -> Match:
         pass
 
     @abstractmethod
     def remove_hanchan_id_by_id(
         self,
-        session,
-        match_id,
-        hanchan_id,
-    ):
+        session: BaseSession,
+        match_id: int,
+        hanchan_id: int,
+    ) -> Match:
         pass
