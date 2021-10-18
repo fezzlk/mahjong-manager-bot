8821e5c443901d4ca269fc835b40259f8b56c16a
fezzlk <fezzlk796@gmail.com>
2021-10-18 23:10:29 +0900

    typing IConfigRepository

--- src/repositories/interfaces/IConfigRepository.py
+++ src/repositories/interfaces/IConfigRepository.py
@@ -1,31 +1,62 @@
 from abc import ABCMeta, abstractmethod
+from domains.Config import Config
+from sqlalchemy.orm.session import Session as BaseSession
 
 
 class IConfigRepository(metaclass=ABCMeta):
     @abstractmethod
-    def find_one_by_target_id_and_key(self, session, target_id, key):
+    def find_one_by_target_id_and_key(
+        self,
+        session: BaseSession,
+        target_id: str,
+        key: str,
+    ) -> Config:
         pass
 
     @abstractmethod
-    def find_all(self, session):
+    def find_all(
+        self,
+        session: BaseSession,
+    ) -> list:
         pass
 
     @abstractmethod
-    def find_by_target_id(self, session, target_id):
+    def find_by_target_id(
+        self,
+        session: BaseSession,
+        target_id: str,
+    ) -> Config:
         pass
 
     @abstractmethod
-    def find_by_ids(self, session, ids):
+    def find_by_ids(
+        self,
+        session: BaseSession,
+        ids: list,
+    ) -> list:
         pass
 
     @abstractmethod
-    def create(self, session, new_config):
+    def create(
+        self,
+        session: BaseSession,
+        new_config: Config,
+    ) -> None:
         pass
 
     @abstractmethod
-    def delete_by_target_id_and_key(self, session, target_id, key):
+    def delete_by_target_id_and_key(
+        self,
+        session: BaseSession,
+        target_id: str,
+        key: str,
+    ) -> None:
         pass
 
     @abstractmethod
-    def delete_by_ids(self, session, ids):
+    def delete_by_ids(
+        self,
+        session: BaseSession,
+        ids: list,
+    ) -> None:
         pass
