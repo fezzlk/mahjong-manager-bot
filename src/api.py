08732dadafd5904d4b6cd295ba3ec934164c6564
fezzlk <fezzlk796@gmail.com>
2021-10-19 14:03:43 +0900

    clean api

--- src/api.py
+++ src/api.py
@@ -1,24 +1,6 @@
-"""
-api
-"""
-
 from server import app
-from use_cases import (
-    hanchans_use_cases,
-)
-from db_setting import Engine
-from models import Results
 
 
 @app.route('/_api/results')
 def api_get_results():
-    """get results
-
-    Returns:
-        data: results list
-    """
-    Results.get_json(Engine)
-    data = hanchans_use_cases.get()
-    print(data)
-    print(type(data))
-    return data
+    print('api is not complete')
