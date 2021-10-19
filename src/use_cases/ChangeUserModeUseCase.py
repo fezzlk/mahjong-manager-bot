d0b27513656fe5c2ed3f79aeb567db1b30a1135d
fezzlk <fezzlk796@gmail.com>
2021-10-19 15:28:21 +0900

    create use case

---
+++ src/use_cases/ChangeUserModeUseCase.py
@@ -0,0 +1,17 @@
+from domains.User import UserMode
+from services import (
+    user_service,
+    reply_service,
+)
+
+
+class ReplyUserModeUseCase:
+
+    def execute(
+        self,
+        line_user_id: str,
+        mode: UserMode,
+    ) -> None:
+        user_service.chmod(line_user_id, mode)
+        reply_service.add_message('処理を中断しました。')
+        
