b6a3d93881b5c8816d3650309025efffee4d1792
fezzlk <fezzlk796@gmail.com>
2021-10-26 16:06:03 +0900

    refactor

--- src/repositories/interfaces/IConfigRepository.py
+++ src/repositories/interfaces/IConfigRepository.py
@@ -5,19 +5,44 @@ from sqlalchemy.orm.session import Session as BaseSession
 
 
 class IConfigRepository(metaclass=ABCMeta):
+
     @abstractmethod
-    def find_one_by_target_id_and_key(
+    def create(
+        self,
+        session: BaseSession,
+        new_config: Config,
+    ) -> Config:
+        pass
+
+    @abstractmethod
+    def delete_by_ids(
+        self,
+        session: BaseSession,
+        ids: List[str],
+    ) -> int:
+        pass
+
+    @abstractmethod
+    def delete_by_target_id_and_key(
         self,
         session: BaseSession,
         target_id: str,
         key: str,
-    ) -> Config:
+    ) -> int:
         pass
 
     @abstractmethod
     def find_all(
         self,
         session: BaseSession,
+    ) -> List[Config]:
+        pass
+
+    @abstractmethod
+    def find_by_ids(
+        self,
+        session: BaseSession,
+        ids: List[str],
     ) -> List[Config]:
         pass
 
@@ -30,34 +55,10 @@ class IConfigRepository(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def find_by_ids(
-        self,
-        session: BaseSession,
-        ids: List[str],
-    ) -> List[Config]:
-        pass
-
-    @abstractmethod
-    def create(
-        self,
-        session: BaseSession,
-        new_config: Config,
-    ) -> None:
-        pass
-
-    @abstractmethod
-    def delete_by_target_id_and_key(
+    def find_one_by_target_id_and_key(
         self,
         session: BaseSession,
         target_id: str,
         key: str,
-    ) -> None:
-        pass
-
-    @abstractmethod
-    def delete_by_ids(
-        self,
-        session: BaseSession,
-        ids: List[str],
-    ) -> None:
+    ) -> Config:
         pass
