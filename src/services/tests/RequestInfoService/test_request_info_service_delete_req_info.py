f8c0317e725a9043ca3d85c4957a2f3121712565
fezzlk <fezzlk796@gmail.com>
2021-11-07 13:56:26 +0900

    update req info service ut

---
+++ src/services/tests/RequestInfoService/test_request_info_service_delete_req_info.py
@@ -0,0 +1,18 @@
+from services.RequestInfoService import RequestInfoService
+from tests.dummies import (
+    generate_dummy_follow_event,
+)
+
+
+def test_success():
+    # Arrange
+    request_info_service = RequestInfoService()
+    follow_event = generate_dummy_follow_event()
+    request_info_service.set_req_info(follow_event)
+
+    # Act
+    request_info_service.delete_req_info()
+
+    # Assert
+    assert request_info_service.req_line_user_id is None
+    assert request_info_service.req_line_group_id is None
