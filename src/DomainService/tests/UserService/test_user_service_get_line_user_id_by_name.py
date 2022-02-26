5ac70b3bbbdf2c5b9c7dc2abf52e6251a0fe8b10
fezzlk <fezzlk796@gmail.com>
2022-02-26 13:38:12 +0900

    use dummies

--- src/DomainService/tests/UserService/test_user_service_get_line_user_id_by_name.py
+++ src/DomainService/tests/UserService/test_user_service_get_line_user_id_by_name.py
@@ -1,39 +1,11 @@
 import pytest
-from DomainModel.entities.User import User, UserMode
 from DomainService import UserService
 from repositories import (
     user_repository, session_scope
 )
+from tests.dummies import generate_dummy_user_list
 
-dummy_users = [
-    User(
-        line_user_name="test_user1",
-        line_user_id="U0123456789abcdefghijklmnopqrstu1",
-        zoom_url="https://us00web.zoom.us/j/01234567891?pwd=abcdefghijklmnopqrstuvwxyz",
-        mode=UserMode.wait,
-        jantama_name="jantama_user1",
-        matches=[],
-        _id=1,
-    ),
-    User(
-        line_user_name="test_user2",
-        line_user_id="U0123456789abcdefghijklmnopqrstu2",
-        zoom_url="https://us00web.zoom.us/j/01234567892?pwd=abcdefghijklmnopqrstuvwxyz",
-        mode=UserMode.wait,
-        jantama_name="jantama_user2",
-        matches=[],
-        _id=2,
-    ),
-    User(
-        line_user_name="test_user2",  # same name with id=2
-        line_user_id="U0123456789abcdefghijklmnopqrstu3",
-        zoom_url="https://us00web.zoom.us/j/01234567893?pwd=abcdefghijklmnopqrstuvwxyz",
-        mode=UserMode.wait,
-        jantama_name="jantama_user3",
-        matches=[],
-        _id=3,
-    ),
-]
+dummy_users = generate_dummy_user_list()[1:4]
 
 
 def test_success_get_line_user_id():
