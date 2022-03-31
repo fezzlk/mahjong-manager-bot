80af9a40e1522348c47b5462c7d14bf3ec6e17d4
fezzlk <fezzlk796@gmail.com>
2022-04-01 01:02:21 +0900

    create UserMatchRepository

---
+++ src/DomainModel/IRepositories/IUserMatchRepository.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta, abstractmethod
+from typing import List
+from DomainModel.entities.UserMatch import UserMatch
+from sqlalchemy.orm.session import Session as BaseSession
+
+
+class IUserMatchRepository(metaclass=ABCMeta):
+
+    @abstractmethod
+    def create(
+        self,
+        session: BaseSession,
+        new_user_match: UserMatch,
+    ) -> UserMatch:
+        pass
+
+    @abstractmethod
+    def find_by_user_ids(
+        self,
+        session: BaseSession,
+        user_ids: List[str],
+    ) -> List[UserMatch]:
+        pass
