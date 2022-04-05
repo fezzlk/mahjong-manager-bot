42f76a6c84a1919d89c3b7a6014af5fc269aab04
fezzlk <fezzlk796@gmail.com>
2022-04-05 23:54:19 +0900

    imprement api

--- src/api.py
+++ src/api.py
@@ -1,28 +1,72 @@
 from flask import Blueprint
-
+from repositories import (
+    session_scope,
+    user_repository,
+    group_repository,
+    hanchan_repository,
+    match_repository,
+    config_repository,
+)
+import json
+from datetime import datetime
 api_blueprint = Blueprint('api_blueprint', __name__, url_prefix='/_api')
+
+
+def expire_encoder(object):
+    if isinstance(object, datetime):
+        return object.isoformat()
 
 
 @api_blueprint.route('/users')
 def api_get_users():
-    print('api is not complete')
+    with session_scope() as session:
+        records = user_repository.find_all(session)
+        return json.dumps(
+            [r.__dict__ for r in records],
+            default=expire_encoder,
+            ensure_ascii=False,
+        )
 
 
 @api_blueprint.route('/groups')
 def api_get_groups():
-    print('api is not complete')
+    with session_scope() as session:
+        records = group_repository.find_all(session)
+        return json.dumps(
+            [r.__dict__ for r in records],
+            default=expire_encoder,
+            ensure_ascii=False,
+        )
 
 
 @api_blueprint.route('/hanchans')
 def api_get_hanchans():
-    print('api is not complete')
+    with session_scope() as session:
+        records = hanchan_repository.find_all(session)
+        return json.dumps(
+            [r.__dict__ for r in records],
+            default=expire_encoder,
+            ensure_ascii=False,
+        )
 
 
 @api_blueprint.route('/matches')
 def api_get_matches():
-    print('api is not complete')
+    with session_scope() as session:
+        records = match_repository.find_all(session)
+        return json.dumps(
+            [r.__dict__ for r in records],
+            default=expire_encoder,
+            ensure_ascii=False,
+        )
 
 
 @api_blueprint.route('/configs')
 def api_get_configs():
-    print('api is not complete')
+    with session_scope() as session:
+        records = config_repository.find_all(session)
+        return json.dumps(
+            [r.__dict__ for r in records],
+            default=expire_encoder,
+            ensure_ascii=False,
+        )
