cec4b6eaa6e2b2880262d30b8d311b551cf2b905
fezzlk <fezzlk796@gmail.com>
2022-04-06 23:31:17 +0900

    common method

--- src/api.py
+++ src/api.py
@@ -1,3 +1,4 @@
+from typing import List
 from flask import Blueprint
 from repositories import (
     session_scope,
@@ -17,56 +18,44 @@ def expire_encoder(object):
         return object.isoformat()
 
 
+def convert_to_json(records: List) -> str:
+    return json.dumps(
+        [r.__dict__ for r in records],
+        default=expire_encoder,
+        ensure_ascii=False,
+    )
+
+
 @api_blueprint.route('/users')
 def api_get_users():
     with session_scope() as session:
         records = user_repository.find_all(session)
-        return json.dumps(
-            [r.__dict__ for r in records],
-            default=expire_encoder,
-            ensure_ascii=False,
-        )
+        return convert_to_json(records)
 
 
 @api_blueprint.route('/groups')
 def api_get_groups():
     with session_scope() as session:
         records = group_repository.find_all(session)
-        return json.dumps(
-            [r.__dict__ for r in records],
-            default=expire_encoder,
-            ensure_ascii=False,
-        )
+        return convert_to_json(records)
 
 
 @api_blueprint.route('/hanchans')
 def api_get_hanchans():
     with session_scope() as session:
         records = hanchan_repository.find_all(session)
-        return json.dumps(
-            [r.__dict__ for r in records],
-            default=expire_encoder,
-            ensure_ascii=False,
-        )
+        return convert_to_json(records)
 
 
 @api_blueprint.route('/matches')
 def api_get_matches():
     with session_scope() as session:
         records = match_repository.find_all(session)
-        return json.dumps(
-            [r.__dict__ for r in records],
-            default=expire_encoder,
-            ensure_ascii=False,
-        )
+        return convert_to_json(records)
 
 
 @api_blueprint.route('/configs')
 def api_get_configs():
     with session_scope() as session:
         records = config_repository.find_all(session)
-        return json.dumps(
-            [r.__dict__ for r in records],
-            default=expire_encoder,
-            ensure_ascii=False,
-        )
+        return convert_to_json(records)
