1a27f26ad7b72b63e139268e98b39a05bd933d18
fezzlk <fezzlk796@gmail.com>
2022-04-08 22:09:22 +0900

    delete test date

--- src/jwt_setting.py
+++ src/jwt_setting.py
@@ -24,38 +24,6 @@ def identity(payload):
         return users[0]
 
 
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-
-users = [
-    User(1, 'user1', 'abcxyz'),
-    User(2, 'user2', 'abcxyz'),
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-
-
-# def authenticate(line_user_name, password):
-#     print('hoghogeg')
-#     user = username_table.get(line_user_name, None)
-#     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-#         return user
-
-
-# def identity(payload):
-#     user_id = payload['identity']
-#     return userid_table.get(user_id, None)
-
-
-
 def register_jwt(app: Flask):
     # JSONのソートを抑止
     app.config['SECRET_KEY'] = 'super-secret'
