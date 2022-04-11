39d45acee3b9a3dbe236ccc7c70e7f1ef56ac84e
fezzlk <fezzlk796@gmail.com>
2022-04-11 19:13:15 +0900

    delete _id from UserMatch Domain model

--- src/DomainModel/entities/UserMatch.py
+++ src/DomainModel/entities/UserMatch.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 
 @dataclass()
 class UserMatch:
-    _id: int
     user_id: int
     match_id: int
 
@@ -11,8 +10,6 @@ class UserMatch:
         self,
         user_id: int,
         match_id: int,
-        _id: int = None,
     ):
-        self._id = _id
         self.user_id = user_id
         self.match_id = match_id
