f7855f0001d17a4cf5f51781de9337f85a78d7a6
fezzlk <fezzlk796@gmail.com>
2022-04-19 09:03:29 +0900

    use mentions

--- src/use_cases/group_line/AddPointByTextUseCase.py
+++ src/use_cases/group_line/AddPointByTextUseCase.py
@@ -1,3 +1,4 @@
+from typing import List
 from DomainService import (
     user_service,
     hanchan_service,
@@ -16,20 +17,17 @@ class AddPointByTextUseCase:
         text: str,
     ) -> None:
         line_group_id = request_info_service.req_line_group_id
+        mention_line_ids = request_info_service.mention_line_ids
 
-        if text[0] == '@':
-            if len(text[1:].split()) >= 2:
-                # ユーザー名に空白がある場合を考慮し、最後の要素をポイント、そのほかをユーザー名として判断する
+        if len(mention_line_ids) > 0:
+            if len(mention_line_ids) == 1 and len(text[1:].split()) >= 2:
+                # ユーザー名に空白がある場合を考慮し、最後の要素をポイントとして判断する
                 point = text[1:].split()[-1]
-                target_user = text[1:(-1 * len(point))].strip()
-                target_line_user_id = user_service.get_line_user_id_by_name(
-                    target_user
-                )
+                target_line_user_id = mention_line_ids[0]
             else:
                 reply_service.add_message(
-                    'ユーザーを指定する場合は「@[ユーザー名] [点数]」と入力してください。')
+                    'ユーザーを指定する場合はメンションをつけてメッセージの末尾に点数を入力してください。1回につき1人を指定するようにしてください。')
                 return
-
         else:
             target_line_user_id = request_info_service.req_line_user_id
             point = text
