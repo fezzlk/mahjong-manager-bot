b35383b77e13f52a39ca0ce333c4a7a35c48971a
fezzlk <fezzlk796@gmail.com>
2022-04-22 22:36:09 +0900

    add test case

--- src/use_cases/personal_line/tests/test_ReplyHistoryUseCase.py
+++ src/use_cases/personal_line/tests/test_ReplyHistoryUseCase.py
@@ -4,7 +4,17 @@ from ApplicationService import (
     request_info_service,
 )
 from DomainModel.entities.User import User, UserMode
-from repositories import session_scope, user_repository
+from DomainModel.entities.Match import Match
+from DomainModel.entities.Hanchan import Hanchan
+from DomainModel.entities.UserMatch import UserMatch
+from repositories import (
+    session_scope,
+    user_repository,
+    match_repository,
+    user_match_repository,
+    hanchan_repository,
+)
+import pytest
 
 dummy_user = User(
     line_user_name="test_user1",
@@ -13,7 +23,36 @@ dummy_user = User(
     mode=UserMode.wait,
     jantama_name="jantama_user1",
     matches=[],
-    _id=1,
+)
+
+dummy_match = Match(
+    line_group_id="G0123456789abcdefghijklmnopqrstu1",
+    hanchan_ids=[1],
+    users=[],
+    status=2,
+)
+
+dummy_user_match = UserMatch(
+    user_id=1,
+    match_id=1,
+)
+
+dummy_hanchan = Hanchan(
+    line_group_id="G0123456789abcdefghijklmnopqrstu1",
+    raw_scores={
+        "U0123456789abcdefghijklmnopqrstu1": 40000,
+        "U0123456789abcdefghijklmnopqrstu2": 30000,
+        "U0123456789abcdefghijklmnopqrstu3": 20000,
+        "U0123456789abcdefghijklmnopqrstu4": 10000,
+    },
+    converted_scores={
+        "U0123456789abcdefghijklmnopqrstu1": 50,
+        "U0123456789abcdefghijklmnopqrstu2": 10,
+        "U0123456789abcdefghijklmnopqrstu3": -20,
+        "U0123456789abcdefghijklmnopqrstu4": -40,
+    },
+    match_id=1,
+    status=1,
 )
 
 
@@ -31,3 +70,21 @@ def test_execute_not_match():
     # Assert
     assert len(reply_service.texts) == 1
     assert reply_service.texts[0].text == '対局履歴がありません。'
+
+
+def test_execute():
+    with pytest.raises(ValueError):
+        # Arrage
+        with session_scope() as session:
+            user_repository.create(session, dummy_user)
+            match_repository.create(session, dummy_match)
+            user_match_repository.create(session, dummy_user_match)
+            hanchan_repository.create(session, dummy_hanchan)
+
+        request_info_service.req_line_user_id = dummy_user.line_user_id
+        use_case = ReplyHistoryUseCase()
+
+        # Act
+        use_case.execute()
+
+        # Assert
