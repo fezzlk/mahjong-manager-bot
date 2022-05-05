7e31d7629203ec332635ddeafbd829689bf73f86
fezzlk <fezzlk796@gmail.com>
2022-05-05 12:23:51 +0900

    fix

--- src/ApplicationService/RequestInfoService.py
+++ src/ApplicationService/RequestInfoService.py
@@ -17,16 +17,19 @@ class RequestInfoService:
     """
 
     def set_req_info(self, event: Event) -> None:
+
         self.req_line_user_id = event.source.user_id
         if event.source.type == 'room':
             self.req_line_group_id = event.source.room_id
         if event.source.type == 'group':
             self.req_line_group_id = event.source.group_id
-        if event.type == 'message' and event.message.mention is not None:
+        if event.type == 'message' and hasattr(
+                event.message, 'mention') and event.message.mention is not None:
             mentionees = event.message.mention.mentionees
             for mentionee in mentionees:
                 self.mention_line_ids.append(mentionee.user_id)
-
+        print('mention_line_ids')
+        print(self.mention_line_ids)
     """
     メッセージ送信元情報の削除
     一つ前のメッセージ送信元の情報が残らないようにするために使う
