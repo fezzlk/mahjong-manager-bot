aa582f0a77b2fd6450e4d0ceb98724ebc346c300
fezzlk <fezzlk796@gmail.com>
2022-05-16 23:46:39 +0900

    calc with yakuman

--- src/use_cases/group_line/MatchFinishUseCase.py
+++ src/use_cases/group_line/MatchFinishUseCase.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 from DomainService import (
     user_service,
     match_service,
@@ -8,7 +8,11 @@ from ApplicationService import (
     request_info_service,
     reply_service,
 )
-from repositories import session_scope, hanchan_repository
+from repositories import (
+    session_scope,
+    hanchan_repository,
+    yakuman_user_repository,
+)
 
 
 class MatchFinishUseCase:
@@ -29,34 +33,60 @@ class MatchFinishUseCase:
         with session_scope() as session:
             hanchans = hanchan_repository.find_by_ids(
                 session, ids)
+            yakuman_users = yakuman_user_repository.find_by_hanchan_ids(
+                session, ids)
 
         total_scores: Dict[str, int] = {}
-        for i in range(len(ids)):
-            converted_scores = hanchans[i].converted_scores
+        total_yakuman_plus: Dict[str, int] = {
+            k: 0 for k in total_scores.keys()}
+        total_yakuman_minus: Dict[str, int] = {
+            k: 0 for k in total_scores.keys()}
+
+        for hanchan in hanchans:
+            converted_scores = hanchan.converted_scores
 
             for line_user_id, converted_score in converted_scores.items():
                 if line_user_id not in total_scores.keys():
                     total_scores[line_user_id] = 0
                 total_scores[line_user_id] += converted_score
 
+            for yu in [
+                    target for target in yakuman_users if target.hanchan_id == hanchan._id]:
+                for line_user_id in converted_scores.keys():
+                    if line_user_id == yu.user_id:
+                        total_yakuman_plus[line_user_id] += 1
+                    else:
+                        total_yakuman_minus[line_user_id] += 1
+
+        results: List[str] = []
+        for line_user_id, converted_score in total_scores.items():
+            yakuman_info = '+' * total_yakuman_plus[line_user_id] \
+                + '-' * total_yakuman_minus[line_user_id]
+            results.append(
+                f'{user_service.get_name_by_line_user_id(line_user_id)}: {converted_score}{yakuman_info}')
+
         reply_service.add_message(
-            '\n'.join([
-                f'{user_service.get_name_by_line_user_id(line_user_id)}: {converted_score}'
-                for line_user_id, converted_score in total_scores.items()
-            ])
+            '\n'.join(results)
         )
 
-        key = 'レート'
-        match_list = []
+        price_list = []
         for line_user_id, converted_score in total_scores.items():
             name = user_service.get_name_by_line_user_id(line_user_id)
-            price = str(
-                converted_score * int(config_service.get_value_by_key(line_group_id, key)[1]) * 10)
-            score = ("+" if converted_score > 0 else "") + str(converted_score)
-            match_list.append(f'{name}: {price}円 ({score})')
+            rate = int(
+                config_service.get_value_by_key(
+                    line_group_id, 'レート')[1])
+            yakuman_prize = 300
+            price = str(converted_score * rate * 10)\
+                + yakuman_prize * 3 * total_yakuman_plus[line_user_id]\
+                - yakuman_prize * total_yakuman_minus[line_user_id]
+            yakuman_info = '+' * total_yakuman_plus[line_user_id]\
+                + '-' * total_yakuman_minus[line_user_id]
+            score = ("+" if converted_score > 0 else "") + \
+                str(converted_score) + yakuman_info
+            price_list.append(f'{name}: {price}円 ({score})')
 
         reply_service.add_message(
-            '対戦ID: ' + str(match_id) + '\n' + '\n'.join(match_list)
+            '対戦ID: ' + str(match_id) + '\n' + '\n'.join(price_list)
         )
 
         match_service.archive(line_group_id)
