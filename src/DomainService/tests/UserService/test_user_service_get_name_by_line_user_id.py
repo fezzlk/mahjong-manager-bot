dc4ba4e2307d0f018755c1b19edc720ec85db731
fezzlk <fezzlk796@gmail.com>
2022-05-21 00:47:51 +0900

    fix test

--- src/DomainService/tests/UserService/test_user_service_get_name_by_line_user_id.py
+++ src/DomainService/tests/UserService/test_user_service_get_name_by_line_user_id.py
@@ -29,12 +29,38 @@ def test_get_from_profile(mocker):
     assert result == dummy_profile.display_name
 
 
-def test_ok_line_api_do_not_reply_id(mocker):
+def test_ok_from_db(mocker):
     # Arrage
     user_service = UserService()
     dummy_users = generate_dummy_user_list()[:3]
     with session_scope() as session:
         for dummy_user in dummy_users:
+            user_repository.create(
+                session=session,
+                new_user=dummy_user,
+            )
+
+    mocker.patch.object(
+        line_bot_api,
+        'get_profile',
+        side_effect=Exception,
+    )
+
+    # Act
+    result = user_service.get_name_by_line_user_id(
+        line_user_id=dummy_users[0].line_user_id,
+    )
+
+    # Assert
+    assert result == dummy_users[0].line_user_name
+
+
+def test_ok_line_api_do_not_reply_id(mocker):
+    # Arrage
+    user_service = UserService()
+    dummy_users = generate_dummy_user_list()[:3]
+    with session_scope() as session:
+        for dummy_user in dummy_users[1:]:
             user_repository.create(
                 session=session,
                 new_user=dummy_user,
