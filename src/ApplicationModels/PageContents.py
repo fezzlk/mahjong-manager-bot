49250d0939dad8a07f5c313b58b099d2d2521a1d
fezzlk <fezzlk796@gmail.com>
2023-03-14 00:56:39 +0900

    create PageContents model

---
+++ src/ApplicationModels/PageContents.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+from flask import Request
+from flask.sessions import SessionMixin
+
+from DomainModel.entities.WebUser import WebUser
+
+
+@dataclass()
+class RegisterFormData:
+    login_name: str
+    login_email: str
+
+    def __init__(
+        self,
+        session: SessionMixin,
+    ):
+        self.login_name = session['login_name']
+        self.login_email = session['login_email']
+
+
+T = TypeVar('T', RegisterFormData, None)
+
+
+@dataclass()
+class PageContents(Generic[T]):
+    session: dict
+    request: Request
+    page_title: str
+    login_user: WebUser
+    line_user_name: str
+    next_page_url: str
+    data: T
+    message: str
+
+    def __init__(
+        self,
+        session: SessionMixin,
+        request: Request,
+        DataClass: T = None,
+        page_title: str = '',
+    ):
+        self.session = dict(session)
+        self.login_user = session.get('login_user', None)
+        self.next_page_url = session.get('next_page_url', '')
+        self.line_user_name = ''
+        self.page_title = page_title
+        self.request = request
+        self.message = ''
+        if DataClass is not None:
+            self.data = DataClass(session)
