a4fc70a063522f568e171b367d3b9dc89d85e1e9
fezzlk <fezzlk796@gmail.com>
2023-08-13 15:23:45 +0900

    test

---
+++ src/DomainService/tests/MatchService/test_match_service_update.py
@@ -0,0 +1,28 @@
+from DomainService import (
+    match_service,
+)
+from repositories import match_repository
+from DomainModel.entities.Match import Match
+
+dummy_matches = [
+    Match(
+        _id=1,
+        line_group_id="G0123456789abcdefghijklmnopqrstu1",
+        status=1,
+    )
+]
+
+
+def test_ok_hit_match(mocker):
+    # Arrange
+    mock_update = mocker.patch.object(
+        match_repository,
+        'update',
+        return_value=1,
+    )
+
+    # Act
+    match_service.update(dummy_matches[0])
+
+    # Assert
+    mock_update.assert_called_once()
---
+++ src/DomainService/tests/UserGroupService/test_user_group_service_find_all_by_line_group_id.py
@@ -0,0 +1,30 @@
+from DomainService import (
+    user_group_service,
+)
+from repositories import user_group_repository
+from DomainModel.entities.UserGroup import UserGroup
+
+dummy_user_group = UserGroup(
+        _id=1,
+        line_group_id="G0123456789abcdefghijklmnopqrstu1",
+        line_user_id="U0123456789abcdefghijklmnopqrstu1",
+    )
+
+
+def test_ok_hit_match(mocker):
+    # Arrange
+    mock_find = mocker.patch.object(
+        user_group_repository,
+        'find',
+        return_value=[dummy_user_group],
+    )
+
+    # Act
+    result = user_group_service.find_all_by_line_group_id("G0123456789abcdefghijklmnopqrstu1")
+
+    # Assert
+    assert len(result) == 1
+    assert result[0]._id == dummy_user_group._id
+    assert result[0].line_group_id == dummy_user_group.line_group_id
+    assert result[0].line_user_id == dummy_user_group.line_user_id
+    mock_find.assert_called_once_with(query={'line_group_id': "G0123456789abcdefghijklmnopqrstu1"})
