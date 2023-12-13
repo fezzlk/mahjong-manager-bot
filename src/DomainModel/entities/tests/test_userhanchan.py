9301a618cb1a01a06376f618d1bd5167806be6eb
fezzlk <fezzlk796@gmail.com>
2023-12-13 13:24:23 +0900

    test entity

---
+++ src/DomainModel/entities/tests/test_userhanchan.py
@@ -0,0 +1,50 @@
+from DomainModel.entities.UserHanchan import UserHanchan
+from datetime import datetime
+from bson.objectid import ObjectId
+
+def test_success():
+    # Arrange
+
+    # Act
+    uh = UserHanchan(
+        line_user_id="U0123456789abcdefghijklmnopqrstu2",
+        hanchan_id=2,
+        point=3,
+        rank=4,
+        yakuman_count=5,
+        _id=1,
+        created_at=datetime(2022, 1, 2, 3, 4, 5),
+        updated_at=datetime(2023, 1, 2, 3, 4, 5),
+    )
+    
+    # Assert
+    assert uh._id == 1
+    assert uh.line_user_id == "U0123456789abcdefghijklmnopqrstu2"
+    assert uh.hanchan_id == 2
+    assert uh.point == 3
+    assert uh.rank == 4
+    assert uh.yakuman_count == 5
+    assert uh.created_at == datetime(2022, 1, 2, 3, 4, 5)
+    assert uh.updated_at == datetime(2023, 1, 2, 3, 4, 5)
+
+
+def test_success_default():
+    # Arrange
+
+    # Act
+    uh = UserHanchan(
+        line_user_id="U0123456789abcdefghijklmnopqrstu2",
+        hanchan_id=2,
+        point=3,
+        rank=4,
+    )
+    
+    # Assert
+    assert uh._id is None
+    assert uh.line_user_id == "U0123456789abcdefghijklmnopqrstu2"
+    assert uh.hanchan_id == 2
+    assert uh.point == 3
+    assert uh.rank == 4
+    assert uh.yakuman_count == 0
+    assert uh.created_at.date() == datetime.now().date()
+    assert uh.updated_at.date() == datetime.now().date()
---
+++ src/DomainModel/entities/tests/test_webuser.py
@@ -0,0 +1,47 @@
+from DomainModel.entities.WebUser import WebUser
+from datetime import datetime
+from bson.objectid import ObjectId
+
+def test_success():
+    # Arrange
+
+    # Act
+    webuser = WebUser(
+        user_code="code",
+        name='name',
+        email='email',
+        _id=1,
+        linked_line_user_id='U0123456789abcdefghijklmnopqrstu2',
+        is_approved_line_user=True,
+        created_at=datetime(2022, 1, 2, 3, 4, 5),
+        updated_at=datetime(2023, 1, 2, 3, 4, 5),
+    )
+    
+    # Assert
+    assert webuser._id == 1
+    assert webuser.user_code == "code"
+    assert webuser.linked_line_user_id == "U0123456789abcdefghijklmnopqrstu2"
+    assert webuser.name == 'name'
+    assert webuser.email == 'email'
+    assert webuser.is_approved_line_user == True
+    assert webuser.created_at == datetime(2022, 1, 2, 3, 4, 5)
+    assert webuser.updated_at == datetime(2023, 1, 2, 3, 4, 5)
+
+
+def test_success_default():
+    # Arrange
+
+    # Act
+    webuser = WebUser(
+        user_code="code",
+    )
+    
+    # Assert
+    assert webuser._id is None
+    assert webuser.user_code == "code"
+    assert webuser.linked_line_user_id is None
+    assert webuser.name is None
+    assert webuser.email is None
+    assert webuser.is_approved_line_user == False
+    assert webuser.created_at.date() == datetime.now().date()
+    assert webuser.updated_at.date() == datetime.now().date()
